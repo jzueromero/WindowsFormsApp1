9ac49312c88ebf4a4604ee7106cd43aeab06bfa2
Josue Romero <rj.josueromero@gmail.com>
2019-05-20 15:09:51 -0600

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mapeo = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.txtTextoInicial = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Mapeo
+            // 
+            this.Mapeo.Location = new System.Drawing.Point(24, 186);
+            this.Mapeo.Name = "Mapeo";
+            this.Mapeo.Size = new System.Drawing.Size(222, 23);
+            this.Mapeo.TabIndex = 0;
+            this.Mapeo.Text = "Mapeo";
+            this.Mapeo.UseVisualStyleBackColor = true;
+            this.Mapeo.Click += new System.EventHandler(this.Mapeo_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Modelo Notify";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(24, 229);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResultado.Size = new System.Drawing.Size(431, 240);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // txtTextoInicial
+            // 
+            this.txtTextoInicial.Location = new System.Drawing.Point(24, 12);
+            this.txtTextoInicial.Multiline = true;
+            this.txtTextoInicial.Name = "txtTextoInicial";
+            this.txtTextoInicial.Size = new System.Drawing.Size(431, 154);
+            this.txtTextoInicial.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 481);
+            this.Controls.Add(this.txtTextoInicial);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Mapeo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Mapeo;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox txtTextoInicial;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Mapeo_Click(object sender, EventArgs e)
+        {
+            string resultado = null;
+            resultado = txtTextoInicial.Text.Replace("{ get; set; }", ",");
+            resultado = resultado.Replace("public", "");
+            resultado = resultado.TrimStart();
+            resultado = resultado.TrimEnd();
+            var array = resultado.Split(',');
+            string ResutaldoFinal = "";
+            int Contador = 0;
+            try
+            {
+                foreach (var i in array)
+                {
+                    var ParValorString = i.Trim();
+                    if (ParValorString.Length > 0)
+                    {
+                        var Par = ParValorString.Split(' ');
+                        ResutaldoFinal += Par[1].ToString() + " = (" + Par[0] + ")item.ItemArray[" + Contador.ToString() + "], \r\n";
+                    }
+                    Contador++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.Message);
+            }
+            Contador = 0;
+            resultado = ResutaldoFinal;
+            txtResultado.Text = resultado;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            #region espacios
+            string resultado = null;
+            resultado = txtTextoInicial.Text.Replace("{ get; set; }", ",");
+            resultado = resultado.Replace("public", "");
+            resultado = resultado.TrimStart();
+            resultado = resultado.TrimEnd();
+            var array = resultado.Split(',');
+            string ResutaldoFinal = "";
+            int Contador = 0;
+            #endregion
+            try
+            {
+                foreach (var i in array)
+                {
+                    var ParValorString = i.Trim();
+                    if (ParValorString.Length > 0)
+                    {
+                        var Par = ParValorString.Split(' ');
+                        ResutaldoFinal += string.Format("private {0} _{1}; {2}  public {0} {1} {2} {3} {2} " +
+                            "get {2} {3} {2} return _{1}; {4} {2} set {3} _{1} = value; {2} OnPropertyChanged(); " +
+                            "{2} {4} {2} {4} ", Par[0].ToString(), Par[1].ToString(), "\r\n", "{", "}");
+                    }
+                    Contador++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Contador = 0;
+            resultado = ResutaldoFinal;
+            txtResultado.Text = resultado;
+        }
+    }
+}
